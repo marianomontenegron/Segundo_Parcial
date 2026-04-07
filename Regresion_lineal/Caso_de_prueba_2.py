99926207ddf308a11e69f50c49b3ff3005ad7bc7
Mariano Montenegro <mariano.montenegro.n@gmail.com>
2026-04-07 12:12:34 -0600

    Proyectoparte1

---
+++ Regresion_lineal/Caso_de_prueba_2.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class CasoPrueba2():
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.xk = 0
+        self.xavg = 0
+        self.yavg = 0
+        self.n = 0
+        self.B1 = 0
+        self.B0 = 0
+        self.r2 = 0
+        self.Yk = 0
+        self.rxy = 0
+
+    def leerDatos(self):
+        self.x = [130,650,99,150,128,302,95,945,368,961]
+        self.y = [15,69.9,6.5,22.4,28.4,65.9,19.4,198.7,38.8,138.2]
+        self.xk = 386
+        self.xavg = np.average(self.x)
+        self.yavg = np.average(self.y)
+        self.n = len(self.x)
+
+    def RealizarCalculo(self):
+
+        self.B1 = ((sum(self.x)*sum(self.y))-(self.n*self.xavg*self.yavg))/((sum(np.pow(self.x,2)))-(self.n*(self.xavg**2)))
+        self.B0 = self.yavg - self.B1
+        self.rxy = (self.n*(sum(self.x)*sum(self.y))-(sum(self.x)*sum(self.y)))/((self.n*(sum(np.pow(self.x,2)))-(sum(self.x)**2))*((self.n*sum(np.pow(self.y,2)))-(sum(self.y)**2)))**(1/2)
+        self.r2 = self.rxy*self.rxy
+        self.Yk = self.B0+self.B1*self.xk
---
+++ Regresion_lineal/Caso_de_prueba_3.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class CasoPrueba3():
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.xk = 0
+        self.xavg = 0
+        self.yavg = 0
+        self.n = 0
+        self.B1 = 0
+        self.B0 = 0
+        self.r2 = 0
+        self.Yk = 0
+        self.rxy = 0
+
+    def leerDatos(self):
+        self.x = [163,765,141,166,137,355,136,1206,433,1130]
+        self.y = [186,699,132,272,291,331,199,1890,788,1601]
+        self.xk = 386
+        self.xavg = np.average(self.x)
+        self.yavg = np.average(self.y)
+        self.n = len(self.x)
+
+    def RealizarCalculo(self):
+
+        self.B1 = ((sum(self.x)*sum(self.y))-(self.n*self.xavg*self.yavg))/((sum(np.pow(self.x,2)))-(self.n*(self.xavg**2)))
+        self.B0 = self.yavg - self.B1
+        self.rxy = (self.n*(sum(self.x)*sum(self.y))-(sum(self.x)*sum(self.y)))/((self.n*(sum(np.pow(self.x,2)))-(sum(self.x)**2))*((self.n*sum(np.pow(self.y,2)))-(sum(self.y)**2)))**(1/2)
+        self.r2 = self.rxy*self.rxy
+        self.Yk = self.B0+self.B1*self.xk
