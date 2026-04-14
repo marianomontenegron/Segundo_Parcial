0bbc5517cdb9f9a83969013d9d59ee21f545789a
Mariano Montenegro <mariano.montenegro.n@gmail.com>
2026-04-14 11:48:35 -0600

    Proyectoparte1.4

--- Regresion_lineal/Caso_de_prueba_3.py
+++ Regresion_lineal/Caso_de_prueba_3.py
@@ -14,6 +14,12 @@ class CasoPrueba3():
         self.r2 = 0
         self.Yk = 0
         self.rxy = 0
+        self.sumx = 0
+        self.sumy = 0
+        self.B1_num = 0
+        self.B1_denom = 0
+        self.r_num = 0
+        self.r_denom = 0
 
     def leerDatos(self):
         self.x = [163,765,141,166,137,355,136,1206,433,1130]
@@ -22,11 +28,17 @@ class CasoPrueba3():
         self.xavg = np.average(self.x)
         self.yavg = np.average(self.y)
         self.n = len(self.x)
+        self.sumx = sum(self.x)
+        self.sumy = sum(self.y)
+        self.B1_num = sum(self.x*self.y)-(self.n*self.xavg*self.yavg)
+        self.B1_denom = sum(np.pow(self.x,2))-self.n*(self.xavg**2)
+        self.r_num = self.n*(sum(self.x*self.y))-(self.sumx*self.sumy)
+        self.r_denom = (self.n*sum(np.pow(self.x,2))-(self.sumx**2))*((self.n*sum(np.pow(self.y,2)))-(self.sumy**2))
 
     def RealizarCalculo(self):
 
-        self.B1 = ((sum(self.x)*sum(self.y))-(self.n*self.xavg*self.yavg))/((sum(np.pow(self.x,2)))-(self.n*(self.xavg**2)))
+        self.B1 = self.B1_num/self.B1_denom
         self.B0 = self.yavg - self.B1
-        self.rxy = (self.n*(sum(self.x)*sum(self.y))-(sum(self.x)*sum(self.y)))/((self.n*(sum(np.pow(self.x,2)))-(sum(self.x)**2))*((self.n*sum(np.pow(self.y,2)))-(sum(self.y)**2)))**(1/2)
+        self.rxy = self.r_num/np.sqrt(self.r_denom)
         self.r2 = self.rxy*self.rxy
         self.Yk = self.B0+self.B1*self.xk
